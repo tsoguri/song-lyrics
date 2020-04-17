a5c6d7c79aaf7d966123a78924308af24d564ea9
Keethu Ramalingam <53497931+keethu-ram@users.noreply.github.com>
2020-04-17 01:58:11 -0600

    Update combine.py

--- combine.py
+++ combine.py
@@ -3,11 +3,19 @@ import csv
 import re
 
 spotify_df = pd.read_csv('songdata.csv', sep=',', engine='python')
-
+billboard_df = pd.read_csv('billboard.csv', sep=',', engine='python')
 
 spotify_df['clean'] = ""
 
 for index, row in spotify_df.iterrows():
     val = (re.sub('[^a-zA-Z.\d\s]', '', row['title'].lower()))
     spotify_df.at[index, 'clean'] = val
-spotify_df.to_csv('new.csv')
+
+billboard_df = billboard_df[billboard_df.Year > 2009]
+
+merged = pd.merge(spotify_df, billboard_df, left_on='clean', right_on='Song', how='left')
+merged = merged.drop_duplicates(subset = 'title', keep = 'first')
+merged = merged.dropna()
+merged = merged.drop(['clean', 'Rank', 'Song', 'Artist', 'Year', 'Source'], axis = 1)
+
+merged.to_csv('new.csv')
